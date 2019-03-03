526ef990558ebb102a863a1133558d6da06bc841
VamsiMohanRamineedi <vmr286@nyu.edu>
2019-03-03 15:07:22 -0500

    Loop through listings in listings.html template and add pagination

--- listings/views.py
+++ listings/views.py
@@ -1,9 +1,19 @@
-from django.shortcuts import render
-
-# Create your views here.
+from django.shortcuts import get_object_or_404, render
+from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
+from .models import Listing
 
 def index(request):
-    return render(request, 'listings/listings.html')
+  listings = Listing.objects.order_by('-list_date').filter(is_published=True)
+
+  paginator = Paginator(listings, 6)
+  page = request.GET.get('page')
+  paged_listings = paginator.get_page(page)
+
+  context = {
+    'listings': paged_listings
+  }
+
+  return render(request, 'listings/listings.html', context)
 
 def listing(request, listing_id):
     return render(request, 'listings/listing.html')
